399674c62f92f2b4b3da2d5342a00ad147f12e95
Goksel Ozardali <gokselozardali@gmail.com>
2024-11-02 16:47:00 +0300

    Enable CORS configuration for frontend origin
    Enable CORS configuration in main.ts for specified frontend origin and HTTP methods, supporting cross-origin requests.

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import * as express from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.use('/webhook', express.raw({ type: 'application/json' }));
 
   await app.listen(3000);
